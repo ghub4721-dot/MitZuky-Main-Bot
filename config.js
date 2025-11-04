6a9365d5345b70213c30f72193215e32605888fd
ghub4721-dot <ghub4721@gmail.com>
2025-11-04 08:11:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "",
+  SESSION_ID: process.env.SESSION_ID || "oMpzBBoL#YjMty_4z6ner1Hv47VYhRLJearqcoD_4SaLFi2zl-E0",
   MONGODB: process.env.MONGODB || "mongodb+srv://sykoboza_db_user:2HMukk1s0yBvHnvT@cluster0.qxdtdec.mongodb.net/",
   OWNER_NUM: process.env.OWNER_NUM || "94721931393",
 };
